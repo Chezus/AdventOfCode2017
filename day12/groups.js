75c67dc14fa931529749000f8567dc583773e899
Kevin <kevin@yaaaaaay.me>
2017-12-12 15:08:40 +0100

    Reordered stuff

--- day12/groups.js
+++ day12/groups.js
@@ -9,6 +9,26 @@ let linksArray = [];
 let groups = [];
 let group = [];
 
+function inGroup(index) {
+    for (grp of groups) {
+        if (grp.includes(index))
+            return true;
+    }
+
+    return false;
+}
+
+function createGroup(index) {
+    if (!group.includes(index))
+        group.push(index);
+
+    for (link of linksArray[index]) {
+        if (!group.includes(link)) {
+            createGroup(link);
+        }
+    }
+}
+
 for (str of unprocessedInput) {
     let subStrArr = str.split(" ");
     linksArray.push(subStrArr.slice(1).map(Number));
@@ -25,25 +45,3 @@ for (let i = 0; i < linksArray.length; i++) {
 
 console.log("Answer part 1: " + group[0].length);
 console.log("Answer part 2: " + groups.length);
-
-function inGroup(index)
-{
-    for (grp of groups) {
-        if (grp.includes(index))
-            return true;
-    }
-
-    return false;
-}
-
-function createGroup(index)
-{
-    if (!group.includes(index))
-        group.push(index);
-    
-    for (link of linksArray[index]) { 
-        if (!group.includes(link)) {
-            createGroup(link);
-        }
-    }
-}
